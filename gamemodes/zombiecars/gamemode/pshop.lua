dce242917158b5cd26e0ea0d71cd1c05f740b1d2
DerpyCoal <coalstudios95@gmail.com>
2017-06-19 12:49:14 -0400

    Accomodating for to be missing file

--- gamemodes/zombiecars/gamemode/pshop.lua
+++ gamemodes/zombiecars/gamemode/pshop.lua
@@ -1,4 +1,15 @@
-include("pshop_items.lua")
+--[[ 
+Hey there buddy, it seems you're snooping in my code. Just kidding, it's okay to be here.
+So, let me give you some information about this place.
+This is the item list. Here you can change the prices, but not what the items actually do. Obviously.
+--]]
+
+local items = {"", "+5 Damage"}
+local prices = {"1", "80"}
+local prices_int = {1, 80}
+
+-- Don't touch this stuff!
+-- https://www.youtube.com/watch?v=8iE8W4iq7X8
 
 function addpoint( ply, pts )
 	ply:AddFrags(pts)
