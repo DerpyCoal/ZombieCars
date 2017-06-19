ef302a08a6f9b9e127f694f928d918771293cf61
DerpyCoal <coalstudios95@gmail.com>
2017-06-19 12:49:41 -0400

    Deleting pointless file.

--- gamemodes/zombiecars/gamemode/pshop_items.lua
+++
@@ -1,9 +0,0 @@
---[[ 
-Hey there buddy, it seems you're snooping in my code. Just kidding, it's okay to be here.
-So, let me give you some information about this place.
-This is the item list. Here you can change the prices, but not what the items actually do. Obviously.
---]]
-
-local items = {"", "+5 Damage"}
-local prices = {"1", "80"}
-local prices_int = {1, 80}
